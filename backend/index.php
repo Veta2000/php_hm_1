019f3840f5b292656966054bc1ca4fc08179cba7
morphey312 <312chernenkods@gmail.com>
2023-11-25 11:47:49 +0200

    Lesson-3. Dealing with Form

--- backend/index.php
+++ backend/index.php
@@ -92,11 +92,11 @@
                 </div>
                 <div class="col-md-7 col-lg-8">
                     <h4 class="mb-3">Billing address</h4>
-                    <form class="needs-validation" novalidate>
+                    <form class="needs-validation" enctype="multipart/form-data" novalidate action="scripts/main.php" method="POST">
                         <div class="row g-3">
                             <div class="col-sm-6">
                                 <label for="firstName" class="form-label">First name</label>
-                                <input type="text" class="form-control" id="firstName" placeholder="" value="" required>
+                                <input type="text" class="form-control" id="firstName" placeholder="" value="" name="firstName" required>
                                 <div class="invalid-feedback">
                                     Valid first name is required.
                                 </div>
@@ -104,7 +104,7 @@
 
                             <div class="col-sm-6">
                                 <label for="lastName" class="form-label">Last name</label>
-                                <input type="text" class="form-control" id="lastName" placeholder="" value="" required>
+                                <input type="text" class="form-control" id="lastName" placeholder="" value="" name="lastName" required>
                                 <div class="invalid-feedback">
                                     Valid last name is required.
                                 </div>
@@ -114,7 +114,7 @@
                                 <label for="username" class="form-label">Username</label>
                                 <div class="input-group has-validation">
                                     <span class="input-group-text">@</span>
-                                    <input type="text" class="form-control" id="username" placeholder="Username" required>
+                                    <input type="text" class="form-control" id="username" name="userName" placeholder="Username" required>
                                     <div class="invalid-feedback">
                                         Your username is required.
                                     </div>
@@ -123,15 +123,23 @@
 
                             <div class="col-12">
                                 <label for="email" class="form-label">Email <span class="text-muted">(Optional)</span></label>
-                                <input type="email" class="form-control" id="email" placeholder="you@example.com">
+                                <input type="email" class="form-control" id="email" name="email" placeholder="you@example.com">
                                 <div class="invalid-feedback">
                                     Please enter a valid email address for shipping updates.
                                 </div>
                             </div>
 
                             <div class="col-12">
+                                <label for="file" class="form-label">Select File <span class="text-muted">(Optional)</span></label>
+                                <input type="file" class="form-control" id="file" name="file" placeholder="select file">
+                                <div class="invalid-feedback">
+                                    Please select file.
+                                </div>
+                            </div>
+
+                            <div class="col-12">
                                 <label for="address" class="form-label">Address</label>
-                                <input type="text" class="form-control" id="address" placeholder="1234 Main St" required>
+                                <input type="text" class="form-control" id="address" name="address" placeholder="1234 Main St" required>
                                 <div class="invalid-feedback">
                                     Please enter your shipping address.
                                 </div>
