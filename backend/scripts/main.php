332cf3d2eac3ca9ee5d4f13675f6645237ea6e18
morphey312 <312chernenkods@gmail.com>
2023-11-25 15:15:50 +0200

    Lesson-4

--- backend/scripts/main.php
+++ backend/scripts/main.php
@@ -1,2 +1,21 @@
 <?php
+// Простір імен " Palmo\Core"
+namespace Palmo\Core;
+
+// Базовий клас "User" у просторі імен " Palmo\Core"
+use Palmo\Core\service\Admin as Administarator;
+
+class User {
+   public function __construct()
+   {
+       echo "User";
+   }
+}
+
+$user = new User();
+echo "<br>";
+$admin = new Administarator();
+
+
+
 
