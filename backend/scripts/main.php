b5b68e4cd2c8a838ba575e92296ad7ae0f5b193c
morphey312 <312chernenkods@gmail.com>
2023-11-25 18:59:13 +0200

    Lesson-5. Sessions

--- backend/scripts/main.php
+++ backend/scripts/main.php
@@ -1,16 +1,34 @@
 <?php
-
+require __DIR__ . '../../vendor/autoload.php';
+session_start();
 use Palmo\Core\service\Db;
 
-$dbh = (new Db())->getHandler();
-$statement = $dbh->query('select * from users');
-$users = $statement->fetchAll();
+// Обробка форми входу
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
 
-print_r($users);
+        $db = new Db();
+        $dbh = $db->getHandler();
+        $stmt = $dbh->prepare("SELECT * FROM users WHERE username = :username");
+        $stmt->bindParam(':username', $username);
+        $stmt->execute();
+        $user = $stmt->fetch();
 
-
-
-
-
-
-
+        if(!empty($user)) {
+            if($password === $user['password']) {
+                $_SESSION['user_id'] = $user['id'];
+                $_SESSION['username'] = $user['username'];
+                header("Location: dashboard.php");
+                exit();
+            } else {
+                $_SESSION['error'] = "Невірний пароль";
+                header("Location: /");
+                exit();
+            }
+        } else {
+            $_SESSION['error'] = "Невірний логін";
+            header("Location: /");
+            exit();
+        }
+    }
